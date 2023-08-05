1f3f078cf30e7920d717ddea82d3ab04b08f3425
Yannik Schmidt <sheppy@atlantishq.de>
2023-08-05 14:59:27 +0200

    wip: more verification functionality

---
+++ ldaptools.py
@@ -0,0 +1,54 @@
+import ldap
+def ldap_accept_verification(verification, app):
+
+    ldap_args = app.config["LDAP_ARGS"]
+
+    if verification.verification_type == "signal":
+
+        ldap_server = ldap_args["LDAP_SERVER"]
+        manager_dn = ldap_args["LDAP_BIND_DN"]
+        manager_pw = ldap_args["LDAP_BIND_PW"]
+        base_dn = ldap_args["LDAP_BASE_DN"]
+
+        # estabilish connection 
+        conn = ldap.initialize(ldap_server) 
+        conn.simple_bind_s(manager_dn, manager_password) 
+  
+        # search in scope # 
+        # TODO: update LDAP here
+        # search_scope = ldap.SCOPE_SUBTREE 
+        # search_results = conn.search_s(base_dn, search_scope, search_filter) 
+        
+        # unbind from connection and return # 
+        conn.unbind_s() 
+        return search_results 
+
+    else:
+        raise NotImplementedError(verification.verification_type)
+
+def get_verifications_for_user(user, app):
+
+    ldap_args = app.config["LDAP_ARGS"]
+
+    if verification.verification_type == "signal":
+
+        ldap_server = ldap_args["LDAP_SERVER"]
+        manager_dn = ldap_args["LDAP_BIND_DN"]
+        manager_pw = ldap_args["LDAP_BIND_PW"]
+        base_dn = ldap_args["LDAP_BASE_DN"]
+
+        # estabilish connection 
+        conn = ldap.initialize(ldap_server) 
+        conn.simple_bind_s(manager_dn, manager_password) 
+  
+        # search in scope # 
+        # TODO: update LDAP here
+        # search_scope = ldap.SCOPE_SUBTREE 
+        # search_results = conn.search_s(base_dn, search_scope, search_filter) 
+        
+        # unbind from connection and return # 
+        conn.unbind_s() 
+        return search_results 
+
+    else:
+        raise NotImplementedError(verification.verification_type)
--- server.py
+++ server.py
@@ -2,11 +2,32 @@ class Verification(db.model):
     
     __tablename__ = "verification"
 
+    challenge_id = Column(String, primary_key=True)
     challenge_secret = Column(String, primary_key=True)
+    dispatch_id = Column(String)
     ldap_user = Column(String)
     phone_number = Column(String)
     verification_type = Column(String) # signal, email
-    status = Column(String) # requesting, dispatching, waiting_for_response, bad_response, finished
+
+    # waiting_for_dispatch, received, waiting_for_response, bad_response, finished
+    status = Column(String)
+
+def update_status(verification):
+
+    if verification.verification_type == "signal":
+        if verification.status == "waiting_for_response":
+            return
+        elif verification.status == "waiting_for_dispatch":
+            r = request.get(app.config["DISPATCH_SERVER"] + "?id={}".format(v.dispatch_id))
+            if r.status == 404:
+                v.status = "waiting_for_response"
+                db.session.commit()
+            else:
+                return
+        else:
+            return # nothing to do
+    else:
+        raise NotImplementedError(verification.verification_type)
 
 
 def email_challenge():
@@ -15,16 +36,93 @@ def email_challenge():
     # # JSON Payload ["VERIFY_EMAIL"]
 
 def signal_challenge():
-    # add uid to db
-    # dispatch signal message
 
-@route("/challenge-response")
+    # add uid to db #
+    challenge_id = secrets.token_url_safe(20)
+    secret = secrets.token_url_safe(3)
+    verification = Verification(challenge_id=challenge_id,
+                                    challenge_secret=secret,
+                                    ldap_user=user,
+                                    phone_number=phone_number,
+                                    verification_type="signal"
+                                    status="waiting_for_dispatch")
+    db.session.add(verification)
+    db.session.commit()
+
+    # send to event dispatcher #
+    message = "Your verification code is {}.\n"
+    message += "If you did not request this code please ignore this message "
+    message += "or report it to root@atlantishq.de."
+    payload = { "users": [user], "message" : message }
+    request.post(app.config["DISPATCH_SERVER"], json=payload )
+
+@route("/challenge-response", method=["GET", "POST"])
 def index():
-    # queried from the signal log watcher
+
+    if flask.request.method = "GET":
+
+        cid = flask.request.args.get("cid")
+        if not challenge_id:
+            return (400, "Missing cid")
+        else:
+            c = db.session.query(Verification).filter(Verification.challenge_id==cid).first()
+            if not c:
+                return (404, "Challenge not found")
+            else:
+                update_status(c)
+                return (200, c.status)
+
+    elif flask.request.method == "POST":
+
+        cid = flask.request.args.get("cid")
+        secrets = flask.request.args.get("secret")
+
+        c = db.session.query(Verification).filter(Verification.challenge_id==cid).first()
+        if not c:
+            return (404, "Challenge not found")
+        elif secret != c.challenge_secret:
+            return (400, "Secret Missmatch")
+        else:
+            ldaptools.ldap_accept_verification(c)
+            db.session.delete(c)
+            db.session.commit()
 
 
-@route("/")
+@app.route("/")
 def index():
     # query configured ldap email + phone
     # query email + phone verification status
     # display as [ email ] [ example@atlantishq.com ] [ verified? ] [ verify now ]
+
+    user = flask.request.headers.get("X-Forwarded-Preferred-Username")
+    verifications = ldaptools.get_verifications_for_user(user, app)
+
+    return flask.render_template("index.html", verifications=verifications)
+
+@app.route("/status")
+def status():
+
+    user = flask.request.headers.get("X-Forwarded-Preferred-Username")
+    verifications = ldaptools.get_verifications_for_user(user, app)
+
+    return json.dumps(verifications, indent=2)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='TM Replay Server',
+                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # general parameters #
+    parser.add_argument("-i", "--interface", default="127.0.0.1", help="Interface to listen on")
+    parser.add_argument("-p", "--port",      default="5000",      help="Port to listen on")
+    parser.add_argument("--dispatch-server", required=True,       help="Dispatche Server")
+    parser.add_argument('--engine', default="sqlite://",
+                              help="e.g. postgresql+psycopg2://user:pass@localhost/dbname")
+    args = parser.parse_args()
+
+    # startup #
+    with app.app_context():
+        create_app()
+
+    app.run(host=args.interface, port=args.port)
