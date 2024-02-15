1a847e369decf62aac7bcd0e1450e3f7bd7d501f
Yannik Schmidt <sheppy@atlantishq.de>
2024-02-15 15:48:41 +0100

    fix: topic api call string

--- ntfy_api.py
+++ ntfy_api.py
@@ -13,20 +13,21 @@ def create(target, token, user_obj):
     r = requests.put(target + "/access-and-user", params=params)
     r.raise_for_status()
 
-def topic(target, token, user_obj):
+def topic(target, token, user):
     '''Create user with a topic ACL'''
 
     params = {
-        "user" : user_obj.user,
+        "user" : user,
         "token" : token,
     }
 
     r = requests.get(target + "/topic", params=params)
     if r.status_code == 404:
         rp = requests.put(target + "/topic", params=params)
-        r.raise_for_status()
+        rp.raise_for_status()
         r = requests.get(target + "/topic", params=params)
 
     r.raise_for_status()
-
-    return topic
+    print(r.content)
+    topic_name = r.json()["topic"]
+    return topic_name
